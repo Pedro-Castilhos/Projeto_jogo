227de75c7dab1815fd30b5a68d713bc52003897d
Pedro-Castilhos <119677959+Pedro-Castilhos@users.noreply.github.com>
2023-03-31 16:22:56 -0300

    Arquivos

---
+++ Nodes/Árvore_v1.gd
@@ -0,0 +1,10 @@
+extends StaticBody2D
+
+onready var arvore := $"." as StaticBody2D
+onready var player := get_tree().get_root().get_node("Mundo").get_node("YSort").get_node("Player")
+
+func _physics_process(_delta):
+	if player.position.y < arvore.position.y and player.position.y > arvore.position.y-210 and player.position.x - 100 <= arvore.position.x and arvore.position.x <= player.position.x + 100:
+		arvore.modulate.a = 0.3
+	else:
+		arvore.modulate.a = 1
